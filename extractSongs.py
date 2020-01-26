7898bd2c91dc52e703a6d95b3004392f591b80a7
tscheer100 <54217979+tscheer100@users.noreply.github.com>
2020-01-26 17:15:13 -0600

    Made a test scraper using another website first

--- extractSongs.py
+++ extractSongs.py
@@ -1,2 +1,20 @@
+
 import requests
 import bs4
+
+
+res = requests.get('https://ascienceenthusiast.com/?fbclid=IwAR1LrRJiZYPc0fbCCLxHpgAofgX6Zq8FTo91ptA0tIs0cyJwsaiknG5MT3I')
+
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+titles = soup.find_all("h3", class_="entry-title")
+
+
+for h3 in titles:
+    print(h3.text)
+
+
+
+print(titles.get_text())
+
+
